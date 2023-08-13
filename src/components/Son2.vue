f205d922887d7850c51ed030b0cef6f2c1f827c7
hower <hower@itcast.cn>
2023-08-13 23:35:41 +0800

    02-vuex-学了state用于提供公共组件数据源,mutations用于修改数据

---
+++ src/components/Son2.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="box">
+    <h2>Son2 子组件</h2>
+    从vuex中获取的值:<label>{{ count }}</label>
+    <br />
+    <button @click="subCount(1)">值 - 1</button>
+    <button @click="subCount(5)">值 - 5</button>
+    <button @click="subCount(10)">值 - 10</button>
+<!--    <button @click="changeCountAction(888)">1秒后改成888</button>-->
+    <button @click="changeTitle('前端程序员')">改标题</button>
+    <hr>
+
+  </div>
+</template>
+
+<script>
+
+import { mapState } from 'vuex'
+
+export default {
+  name: 'Son2Com',
+  computed: {
+    ...mapState(['count', 'title'])
+  },
+  methods: {
+    subCount (n) {
+      this.$store.commit('subCount', n)
+    },
+
+    changeTitle () {
+      this.$store.commit('changeTitle', '黑马程序员')
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+.box {
+  border: 3px solid #ccc;
+  width: 400px;
+  padding: 10px;
+  margin: 20px;
+}
+h2 {
+  margin-top: 10px;
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,46 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>
+      根组件
+      - {{ title }}
+      - {{ count }}
+    </h1>
+    <input :value="count" @input="handleInput" type="text">
+    <Son1></Son1>
+    <hr>
+    <Son2></Son2>
   </div>
 </template>
+
+<script>
+import Son1 from '@/components/Son1.vue'
+import Son2 from '@/components/Son2.vue'
+import { mapState } from 'vuex'
+
+export default {
+  name: 'app',
+  computed: {
+    ...mapState(['count', 'title'])
+  },
+  methods: {
+    handleInput (e) {
+      const num = +e.target.value
+      this.$store.commit('changeCount', num)
+    }
+  },
+  components: {
+    Son1,
+    Son2
+  }
+}
+</script>
+
+<style scoped>
+.about {
+  width: 600px;
+  margin: 20px auto;
+  border: 3px solid #ccc;
+  border-radius: 3px;
+  padding: 10px;
+}
+</style>
